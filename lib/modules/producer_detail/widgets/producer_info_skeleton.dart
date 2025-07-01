d3e41fe1cd3302c15c1a2af896a52ce723cc9ad7
Dênym <denym.aq.filho@gmail.com>
2025-07-01 19:58:36 -0400

    [FEAT] carregamento da tela de produtor

--- lib/modules/producer_detail/widgets/producer_info_skeleton.dart
+++ lib/modules/producer_detail/widgets/producer_info_skeleton.dart
@@ -7,39 +7,37 @@ class ProducerInfoSkeleton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return DefaultShimmer(
-      child: Expanded(
-        child: Padding(
-          padding: const EdgeInsets.all(50.0),
-          child: Column(
-            children: [
-              Container(
-                height: 24,
-                width: 300,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  color: Colors.grey,
-                ),
+      child: Padding(
+        padding: const EdgeInsets.all(50.0),
+        child: Column(
+          children: [
+            Container(
+              height: 24,
+              width: 300,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: Colors.grey,
               ),
-              SizedBox(height: 16),
-              Container(
-                height: 14,
-                width: 450,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  color: Colors.grey,
-                ),
+            ),
+            SizedBox(height: 16),
+            Container(
+              height: 14,
+              width: 450,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: Colors.grey,
               ),
-              SizedBox(height: 8),
-              Container(
-                height: 14,
-                width: 450,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  color: Colors.grey,
-                ),
+            ),
+            SizedBox(height: 8),
+            Container(
+              height: 14,
+              width: 450,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: Colors.grey,
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
